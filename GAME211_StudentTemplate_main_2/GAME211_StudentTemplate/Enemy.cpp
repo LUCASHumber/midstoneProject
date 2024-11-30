b1743a7a3ba8b60915ed61b2221d0f653fe6fc5c
LUCASHumber <145156581+LUCASHumber@users.noreply.github.com>
2024-11-29 21:21:50 -0500

    fixes

--- GAME211_StudentTemplate_main_2/GAME211_StudentTemplate/Enemy.cpp
+++ GAME211_StudentTemplate_main_2/GAME211_StudentTemplate/Enemy.cpp
@@ -2,7 +2,7 @@
 
 bool Enemy::OnCreate()
 {
-    image = IMG_Load("Blinky.png");
+    image = IMG_Load("Enemy_4short.png");
     if (image == nullptr) {
         std::cerr << "Can't open the image: " << IMG_GetError() << std::endl;
         return false;
